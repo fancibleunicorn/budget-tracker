f054f72957bb2ed3ff7dbb462a2d32b639fcbe92
Adam Crandall <adamcrandall91@gmail.com>
2021-02-26 14:28:36 -0500

    added css and js files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,10 @@ const DATA_CACHE_NAME = 'data-cache-v1';
 const FILES_TO_CACHE = [
   '/',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './css/style.css',
+  './js/idb.js',
+  './index/index.js'
   
 ];
 
