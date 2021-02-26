8f14c4108699cde503e7f22ddf33b5f8cb7d2d3c
Adam Crandall <adamcrandall91@gmail.com>
2021-02-26 14:20:49 -0500

    updated FILES TO CACHE in service work

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,24 +5,9 @@ const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
   '/',
-  '/index.html',
-  '/favicon.ico',
-  '/manifest.json',
-  '/assets/css/style.css',
-  '/assets/js/loadImages.js',
-  '/assets/images/icons/icon-72x72.png',
-  '/assets/images/icons/icon-96x96.png',
-  '/assets/images/icons/icon-128x128.png',
-  '/assets/images/icons/icon-144x144.png',
-  '/assets/images/1.jpg',
-  '/assets/images/2.jpg',
-  '/assets/images/3.jpg',
-  '/assets/images/4.jpg',
-  '/assets/images/5.jpg',
-  '/assets/images/6.jpg',
-  '/assets/images/7.jpg',
-  '/assets/images/8.jpg',
-  '/assets/images/9.jpg'
+  './index.html',
+  './manifest.json'
+  
 ];
 
 // Install the service worker
